1d7835acafb101ddd65fd2c781fcb23778d40719
Tom Harris <mrtom.h.84@gmail.com>
2017-07-02 19:45:55 +0100

    Add gulp tasks for asset building

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 const gulp = require('gulp');
+const util = require('gulp-util');
 const eslint = require('gulp-eslint');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-clean-css');
+const inject = require('gulp-inject');
+const clean = require('gulp-clean');
+
+const production = !!util.env.production;
+
+gulp.task('clean', () => (
+  gulp.src(['./public/css/**/*.css', './public/js/**/*.js'], { read: false })
+    .pipe(clean())
+));
 
 gulp.task('lint', () => (
   gulp.src(['**/*.js', '!node_modules/**'])
@@ -9,9 +21,11 @@ gulp.task('lint', () => (
     .pipe(eslint.failAfterError())
 ));
 
-gulp.task('sass', () => (
+gulp.task('sass', ['clean'], () => (
   gulp.src('./resources/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(production ? concat('all.min.css') : util.noop())
+    .pipe(production ? minifyCSS() : util.noop())
     .pipe(gulp.dest('./public/css'))
 ));
 
@@ -19,6 +33,18 @@ gulp.task('sass:watch', () => (
   gulp.watch('./sass/**/*.scss', ['sass'])
 ));
 
-gulp.task('default', ['lint', 'sass'], () => {
+gulp.task('inject-css', ['sass'], () => (
+  gulp.src('./resources/views/layouts/injected-css.hbs')
+    .pipe(inject(gulp.src(['./public/css/*.css'], { read: false })))
+    .pipe(gulp.dest('./resources/views/layouts/'))
+));
+
+gulp.task('inject-js', ['clean'], () => (
+  gulp.src('./resources/views/layouts/injected-js.hbs')
+    .pipe(inject(gulp.src(['./public/js/*.js'], { read: false })))
+    .pipe(gulp.dest('./resources/views/layouts/'))
+));
+
+gulp.task('default', ['lint', 'sass', 'inject-css', 'inject-js'], () => {
   //
 });
